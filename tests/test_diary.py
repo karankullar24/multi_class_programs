8069040250e836a9636646d73e3632d92a19baf0
Karan Kullar <jaskaran.kullar.13@alumni.ucl.ac.uk>
2025-08-18 10:30:13 +0100

    added tests for 100% coverage on exceptions

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -1,23 +1,19 @@
 from lib.diary import *
 import pytest
 
-"""
-Initially
-There are no entries
-"""
-
-def test_no_entries():
-    diary = Diary()
-    diary.all() == []
 
 def test_best_entry_with_no_entry_raises_exception():
     diary = Diary()
     with pytest.raises(Exception) as e:
-        diary.reading_time(1)
+        diary.find_best_entry_for_reading_time(1,1)
     error_message = str(e.value)
     assert error_message == "No entries"
 
-def test_reading_time_no_entries_raises_exception():
+def test_all_with_no_entries_returns_empty_list():
+    diary = Diary()
+    assert diary.all() == []
+
+def test_reading_time_with_no_entry_raises_exception():
     diary = Diary()
     with pytest.raises(Exception) as e:
         diary.reading_time(1)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -15,7 +15,17 @@ def test_can_add_diary_entries_to_diary():
     diary.add(entry1)
     diary.add(entry2)
     diary.add(entry3)
-    assert diary.all() == [entry1,entry2,entry3]
+    assert diary.entries == [entry1,entry2,entry3]
+
+def test_can_list_with_all_method():
+    diary = Diary()
+    entry1 = DiaryEntry("Title 1","My first diary entry")
+    entry2 = DiaryEntry("Title 2","My second diary entry that's got more in it to read")
+    entry3 = DiaryEntry("Title 3", "My third diary entry that has considerably more to read because I'm so happy that my multi-class program is working so well. How amazing is that, oh my god.")
+    diary.add(entry1)
+    diary.add(entry2)
+    diary.add(entry3)
+    assert diary.all() == [entry1,entry2,entry3]    
 
 def test_can_count_words_of_all_entries():
     diary = Diary()
